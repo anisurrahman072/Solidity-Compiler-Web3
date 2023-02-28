2c46b4dc6ddbff482fa85a77a8be56082879e8c0
anisurrahman072 <anisurrahmanbup@gmail.com>
2023-03-01 02:56:15 +0600

    Total supply to 1 Million

--- contracts/Kidcoin.sol
+++ contracts/Kidcoin.sol
@@ -5,6 +5,6 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Kidcoin is ERC20 {
     constructor() ERC20("Kidcoin", "KIDCOIN") {
-        _mint(msg.sender, 1000 * (10**decimals()));
+        _mint(msg.sender, 1000000 * (10**decimals()));
     }
 }

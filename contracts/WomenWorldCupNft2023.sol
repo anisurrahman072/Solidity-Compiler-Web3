bd057b903e1654430d8f2dd470f484971ccd2136
anisurrahman072 <anisurrahmanbup@gmail.com>
2023-06-05 13:37:01 +0600

    File name modified

--- contracts/WomenWorldCupNft2023.sol
+++ contracts/WomenWorldCupNft2023.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract WomenWorldCupNft is ERC721URIStorage {
+contract WomenWorldCupNft2023 is ERC721URIStorage {
     address private owner;
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
